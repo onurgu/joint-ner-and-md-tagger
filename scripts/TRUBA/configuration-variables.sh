b12f9b2f9d0545cd50c4205d6934a19d447927c7
Onur Gungor <onurgu@gmail.com>
2018-07-30 23:37:57 +0300

    Helper script for TRUBA is updated to be initialized with a configuration file.

--- scripts/TRUBA/configuration-variables.sh
+++ scripts/TRUBA/configuration-variables.sh
@@ -2,4 +2,7 @@
 export ner_tagger_root=/truba/home/ogungor/projects/research/projects/focus/joint_md_and_ner/joint-ner-and-md-tagger
 export virtualenv_name=joint_ner_dynet
 export datasets_root=~/projects/research/datasets/joint_ner_dynet/
-export experiment_logs_path=`pwd`/experiment-logs/
+export experiment_logs_path=/truba/home/ogungor/projects/research/projects/focus/joint_md_and_ner/experiment-logs/
+export virtualenvwrapper_path=/truba/home/ogungor/.local/bin/virtualenvwrapper.sh
+export environment_variables_path=environment-variables
+export sacred_args='-F '${experiment_logs_path}
