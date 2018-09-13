2acb661c29076ee8b52e9b43fe4b5b38721f662b
Onur Gungor <onurgu@gmail.com>
2018-09-13 11:15:55 +0300

    conllu2conllu_with_all_analyses scripts

---
+++ scripts/converters/conllu2conllu_with_all_analyses.py
@@ -0,0 +1,34 @@
+import subprocess
+import sys
+
+assert len(sys.argv) == 3, \
+    "You need to supply two command line arguments: model file path and input file in CONLLU format"
+
+model_filepath=sys.argv[1]
+input_filepath=sys.argv[2]
+
+# command_template = "/Users/onur/Desktop/projects/research/projects/focus/morphology/udpipe/cmake-build-debug/udpipe " \
+#                    "/Users/onur/Desktop/projects/research/projects/focus/morphology/data/hungarian-ud-2.0-170801.udpipe " \
+#                    "/Users/onur/Desktop/projects/research/datasets/hungarian/UD_Hungarian-Szeged/hu_szeged-ud-test.conllu " \
+#                    "--input=conllu --tag --tagger=provide_all_analyses --output=conllu " \
+#                    "--outfile=denem"
+
+UDPIPE_COMMAND_PATH="/Users/onur/Desktop/projects/research/projects/focus/morphology/udpipe/cmake-build-debug/udpipe"
+
+command_template = "{UDPIPE_COMMAND_PATH} " \
+                   "{model_filepath} " \
+                   "{input_filepath} " \
+                   "--input=conllu --tag --tagger=provide_all_analyses --output=conllu " \
+                   "--outfile={output_filepath}"
+
+# print(command_template.format(UDPIPE_COMMAND_PATH=UDPIPE_COMMAND_PATH,
+#                                                 model_filepath=model_filepath,
+#                                                 input_filepath=input_filepath,
+#                                                 output_filepath=input_filepath+".all_analyses").split(" "))
+
+result = subprocess.check_output(command_template.format(UDPIPE_COMMAND_PATH=UDPIPE_COMMAND_PATH,
+                                                model_filepath=model_filepath,
+                                                input_filepath=input_filepath,
+                                                output_filepath=input_filepath+".all_analyses").split(" "))
+
+print(result)
---
+++ scripts/converters/conllu2conllu_with_all_analyses_runner.py
@@ -0,0 +1,64 @@
+
+import os
+import sys
+import subprocess
+
+script_path = os.path.join(os.getcwd(), os.path.dirname(sys.argv[0]))
+
+try:
+    import ConfigParser
+    cp = ConfigParser
+except ImportError as e:
+    print(e)
+    import configparser
+    cp = configparser
+
+assert len(sys.argv) == 2, "You should input the datasets_dirpath"
+
+lang_names = ["czech",
+              "finnish",
+              "hungarian",
+              "spanish",
+              "turkish"]
+
+datasets_dirpath = sys.argv[1]
+
+ini_filepath_template = "{lang_name}-joint-md-and-ner-tagger.ini"
+
+for lang_name in lang_names:
+    ini_filepath = ini_filepath_template.format(lang_name=lang_name)
+
+    c_parser = cp.ConfigParser()
+
+    lang_dirpath = os.path.join(datasets_dirpath, lang_name)
+
+    c_parser.read([os.path.join(lang_dirpath, ini_filepath)])
+
+    model_path = c_parser.get("udpipe", "model")
+
+    train_filepaths = []
+
+    # ner_train_filepath = c_parser.get("ner", "train_file")
+    #
+    # print(model_path, ner_train_filepath)
+
+    command_template = "python " \
+                       "{script_path}/conllu2conllu_with_all_analyses.py " \
+                       "{model_path} " \
+                       "{input_filepath}"
+
+    sections = "ner md".split(" ")
+
+    dataset_filepath_labels = "train_file dev_file test_file".split(" ")
+
+    for section in sections:
+        for dataset_filepath_label in dataset_filepath_labels:
+            try:
+                dataset_filepath = c_parser.get(section, dataset_filepath_label)
+                command = command_template.format(script_path=script_path,
+                                                  model_path=os.path.join(lang_dirpath, model_path),
+                                                  input_filepath=os.path.join(lang_dirpath, dataset_filepath))
+                print(command)
+                subprocess.check_output(command.split(" "))
+            except ConfigParser.NoOptionError as e:
+                print(e)
