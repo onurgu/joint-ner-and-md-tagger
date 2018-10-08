416bd13bea2b9a7e7a73a72f81a694367b8e644e
Onur Gungor <onurgu@gmail.com>
2018-10-08 22:59:11 +0300

    Two enhancements to ini_parse.py

--- utils/ini_parse.py
+++ utils/ini_parse.py
@@ -13,6 +13,10 @@ parser = argparse.ArgumentParser("ini_parse.py")
 
 parser.add_argument("--input", required=True)
 
+parser.add_argument("--only_values", default=False, action="store_true")
+
+parser.add_argument("--add_suffixes", default=False, action="store_true")
+
 parser.add_argument("--query", nargs="*")
 
 args = parser.parse_args()
@@ -29,7 +33,14 @@ for query in args.query:
     tokens = query.split(".")
 
     try:
-        dataset_filepath = "_".join(tokens) + "=" + c_parser.get(tokens[0], tokens[1])
+        dataset_filepath = c_parser.get(tokens[0], tokens[1])
+        if args.add_suffixes:
+            if tokens[0] == "ner":
+                dataset_filepath += ".all_analyses.tagged"
+            elif tokens[0] == "md":
+                dataset_filepath += ".all_analyses"
+        if not args.only_values:
+            dataset_filepath = "_".join(tokens) + "=" + dataset_filepath
 
         # print(dataset_filepath)
         result_str.append(dataset_filepath)
