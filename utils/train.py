bugfix: Log average loss over the training set for each epoch

utils/train.py
@@ -120,7 +120,8 @@ def train(sys_argv):
                     logging.error("BEEP")
 
         print("")
-        print("Avg. loss over training set: {epoch_loss_mean}".format(epoch_loss_mean=np.mean(epoch_costs)))
+        print("Epoch {epoch_no} Avg. loss over training set: {epoch_loss_mean}".format(epoch_no=epoch_no,
+                                                                                       epoch_loss_mean=np.mean(epoch_costs)))
 
         model.trainer.status()
 